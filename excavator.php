25d028cb82fdf29f8aeb5ed0697279e0c3b69a60
Avishkar Autar <aautar@digital-radiation.com>
2016-09-21 23:16:36 -0400

    start work on excavator

---
+++ excavator.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+
+echo "Excavator 1.0.0\n";
+
+$s3Bucket = getenv('S3_BUCKET');
+$s3AccessKey = getenv('S3_ACCESS_KEY');
+$s3SecretKey = getenv('S3_SECRET_KEY');
+$s3Region = getenv('S3_REGION');
+
+if(!isset($argv[1])) {
+    echo "Missing argument\n";   
+    exit;
+}
+
+if(empty($s3Bucket) || empty($s3AccessKey) || empty($s3SecretKey) || empty($s3Region)) {
+    echo "Missing require environment variables\n";
+    exit;
+}
+
+echo 'S3_BUCKET=' . $s3Bucket . "\n";
+echo 'S3_ACCESS_KEY=' . $s3AccessKey . "\n";
+echo 'S3_REGION=' . $s3Region . "\n";
+
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => $s3Region
+]);
+
+$result = $s3->getObject([
+    'Bucket' => $s3Bucket,
+    'Key' => $argv[1]
+]);
+
+var_dump($result);

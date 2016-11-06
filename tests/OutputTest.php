46cd03e056b0ad6e7c3f92f908040c5f87496d3e
Avishkar Autar <aautar@digital-radiation.com>
2016-11-06 14:29:11 -0500

    tests for OutputTest

---
+++ tests/OutputTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Excavator\Tests;
+
+use Excavator\Output;
+
+class OutputTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWriteLineEchosMessage()
+    {
+        $stdout = new Output();
+
+        ob_start();
+        $stdout->writeLine("testing 1 2 3");
+        $outputMessage = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertEquals("testing 1 2 3\n", $outputMessage);
+    }
+
+    public function testWriteMessageStartEchoMessageWithoutNewLine()
+    {
+        $stdout = new Output();
+
+        ob_start();
+        $stdout->writeMessageStart("testing 1 2 3");
+        $outputMessage = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertEquals("testing 1 2 3", $outputMessage);
+    }
+
+    public function testWriteMessageEndEchoMessageWithNewLine()
+    {
+        $stdout = new Output();
+
+        ob_start();
+        $stdout->writeMessageEnd("done.");
+        $outputMessage = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertEquals("done.\n", $outputMessage);
+    }
+}

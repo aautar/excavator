edceb1bfd3b7c4cd8f1d9ad6fa817a9f9b64779f
Avishkar Autar <aautar@digital-radiation.com>
2016-11-08 20:51:16 -0500

    test for DataMigrator::getDatabaseName

--- tests/DataMigratorTest.php
+++ tests/DataMigratorTest.php
@@ -13,4 +13,10 @@ class DataMigratorTest extends \PHPUnit_Framework_TestCase
         $this->expectException(InvalidConnectionStringException::class);
         new DataMigrator("bad-conn-string", "/sql", $this->createMock(Artifact::class));
     }
+
+    public function testGetDatabaseNameReturnsDBName()
+    {
+        $db = new DataMigrator("mysql://root:rootpass@localhost:3306/a_database_123", "/sql", $this->createMock(Artifact::class));
+        $this->assertEquals("a_database_123", $db->getDatabaseName());
+    }
 }

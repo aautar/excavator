6d95cc7cdcdd2c1f7c5d8d549939af2550fbb826
Avishkar Autar <aautar@digital-radiation.com>
2020-12-06 19:12:44 -0500

    fix/update create-phar script, faster and includes vendor deps

--- create-phar.php
+++ create-phar.php
@@ -1,29 +1,100 @@
 <?php
 
-$pharFile = 'excavator.phar';
+function getFilesInDirectory(string $path): array {
+    $files = [];
+    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
 
-if (file_exists($pharFile))  {
-    unlink($pharFile);
+    foreach ($rii as $file) {
+        if ($file->isDir()){ 
+            continue;
+        }
+    
+        $files[] = $file->getPathname();    
+    }
+
+    return $files;
 }
 
-$phar = new Phar($pharFile);
-$phar->startBuffering();
-$defaultStub = $phar->createDefaultStub('excavator.php');
+function removeFilesInDirectory(string $path): array {
+    $files = [];
+    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS));
 
-$phar->addFile(__DIR__ . '/excavator.php', 'excavator.php');
-$phar->addEmptyDir("src");
-$files = glob(__DIR__ . "/src/*.*");
-foreach($files as $f) {
-    $phar->addFile($f, "src/" . basename($f));
+    foreach ($rii as $file) {
+        if ($file->isDir()){
+            continue;
+        }
+    
+        $fp = $file->getPathname();
+        $files[] = $fp;
+        unlink($fp);
+    }
+
+    return $files;
 }
 
-$stub = "#!/usr/bin/php \n" . $defaultStub;
-$phar->setStub($stub);
+function removeEmptyDirectoriesInDirectory(string $path) {
+    $files = [];
+    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
 
-$phar->stopBuffering();
+    foreach ($rii as $f) {
+        if ($f->isDir()){
+            rmdir($f);
+        }
+    }
+}
 
-$phar->compressFiles(Phar::GZ);
+function createPhar() {
+    $pharFile = 'excavator.phar';
 
-chmod(__DIR__ . '/excavator.phar', 0770);
+    if (file_exists($pharFile))  {
+        unlink($pharFile);
+    }
 
-echo "$pharFile successfully created" . PHP_EOL;
+    $phar = new Phar($pharFile);
+    $phar->startBuffering();
+    $defaultStub = $phar->createDefaultStub('excavator.php');
+
+    mkdir(__DIR__ . "/phar-staging");
+
+    copy(__DIR__ . '/excavator.php', __DIR__ . "/phar-staging/excavator.php");
+
+    // Add src files
+    $files = getFilesInDirectory(__DIR__ . "/src");
+    foreach($files as $f) {
+        $pathInPhar = substr($f, strlen(__DIR__) + 1);
+        echo "copying to phar-staging: {$pathInPhar}...\n";
+        @mkdir(dirname(__DIR__ . "/phar-staging/{$pathInPhar}"), 0777, true);
+        copy($f, __DIR__ . "/phar-staging/{$pathInPhar}");
+    }
+
+    // Add vendor files
+    $phar->addEmptyDir("vendor");
+    $files = getFilesInDirectory(__DIR__ . "/vendor");
+    foreach($files as $f) {
+        $pathInPhar = substr($f, strlen(__DIR__) + 1);
+        echo "adding to phar-staging: {$pathInPhar}...\n";
+        @mkdir(dirname(__DIR__ . "/phar-staging/{$pathInPhar}"), 0777, true);
+        copy($f, __DIR__ . "/phar-staging/{$pathInPhar}");
+    }
+
+    echo "creating excavator.phar...\n";
+    $phar->buildFromDirectory(__DIR__ . "/phar-staging");
+
+    $stub = "#!/usr/bin/php \n" . $defaultStub;
+    $phar->setStub($stub);
+
+    $phar->stopBuffering();
+
+    $phar->compressFiles(Phar::GZ);
+
+    chmod(__DIR__ . '/excavator.phar', 0770);
+
+    echo "cleanup up...\n";
+    removeFilesInDirectory(__DIR__ . "/phar-staging");
+    removeEmptyDirectoriesInDirectory(__DIR__ . "/phar-staging");
+    rmdir(__DIR__ . "/phar-staging");
+
+    echo "$pharFile successfully created" . PHP_EOL;
+}
+
+createPhar();
